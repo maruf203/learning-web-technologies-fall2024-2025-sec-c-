be5431422ea9c46f183560ab4b23e981d587a370
Abdullah Al Maruf <107000734+maruf203@users.noreply.github.com>
2024-12-31 02:36:38 +0600

    Update Healthcare.php

--- Final Project/Model/Healthcare.php
+++ Final Project/Model/Healthcare.php
@@ -1,1 +1,47 @@
+<?php
 
+
+class healthcare{
+public  $DBHostName="";
+public $DBUserName="";
+public $DBPassword="";
+public $DBName="";
+function __construct(){
+ $this->DBHostName="localhost";
+ $this->DBUserName="root";
+ $this->DBPassword="";
+ $this->DBName="healthcare";
+}
+
+function createConObject(){
+    return new mysqli($this->DBHostName, $this->DBUserName, $this->DBPassword, 
+    $this->DBName);
+}
+
+function insertOrder($conn,$table,$Name, $Email, $Password){
+$qrystring="INSERT INTO $table (Name,Email,Password) 
+VALUES (  '$Nmae',   '$Email', '$password' )";
+$result = $conn->query($qrystring);
+if($result === false)
+{
+    return $conn->error;
+}
+else{
+    return $result;
+}
+}
+
+
+
+
+function closeCon($conn)
+{
+ $conn->close();
+}
+
+}
+
+
+
+
+?>
